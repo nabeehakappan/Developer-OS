94b946c469c00cb53b3631e9a920bf6a229c91ed
nabeehakappan <nabeehakappan.work@gmail.com>
2026-04-20 20:53:04 +0530

    Finalized frontend showcase

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -13,4 +13,14 @@ API.interceptors.request.use((req) => {
   return req;
 });
 
+axios.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
+});
+
 export default API;
